e63887fbdf06282fbd64708e06c39a9a183e6662
Krishnakhaladkar27 <154739644+Krishnakhaladkar27@users.noreply.github.com>
2024-03-06 12:40:10 +0530

    Add files via upload

---
+++ Program167.c
@@ -0,0 +1,30 @@
+program167.c
+#include<stdio.h>
+
+int CountChar(char *str)
+{
+    int iCount = 0;
+
+    while(*str != '\0')
+    {
+        if(*str == 'z')
+        {
+            iCount++;
+        }
+        str++;
+    }
+    return iCount;
+}
+int main()
+{
+    char Arr[20];
+    int iRet = 0;
+
+    printf("Enter string : \n");
+    scanf("%[^'\n']s",Arr);    
+
+    iRet = CountChar(Arr);    // strlenX(100);
+
+    printf("Number of z are : %d\n",iRet);
+    return 0;
+}
